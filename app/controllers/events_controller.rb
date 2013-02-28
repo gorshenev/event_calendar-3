fffb78d07c979e6c99468c254faae07128dbe3de
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-02-27 18:09:23 -0600

    calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
+    @date = params[:month] ? Date.parse(params[:month]) : Date.today
 
     respond_to do |format|
       format.html # index.html.erb
