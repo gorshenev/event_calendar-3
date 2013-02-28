57773a93ecdbac16caeb77d99bcc848c4b8226f8
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-02-28 16:21:42 -0600

    authorized

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,6 +3,11 @@ class Permission < Struct.new(:user)
     if user.nil? || !user.authorized?
       return true if controller == "sesions" && action.in?(%w[create failed])
       return true if controller == "home"
+      return false if controller == "user_roles" && action.in?(%w[index show edit new])
+      return false if controller == "events" && action.in?(%w[index show edit new])
+      return false if controller == "available_users" && action.in?(%w[index show edit new])
+      return false if controller == "banned_users" && action.in?(%w[index show edit new])
+      return false if controller == "users" && action.in?(%w[index show edit new])
     elsif user.authorized?
        return true if controller == "users" && action.in?(%w[index show edit new])
        return true if controller == "user_roles" && action.in?(%w[index show edit new])
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
-      user.authorized = 'false'
+      if !user.authorized?
+        user.authorized = 'false'
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
