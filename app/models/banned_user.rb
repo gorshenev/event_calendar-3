4ecf5d3848719c25848db6b71e940d35ca1c8faa
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-02-28 15:38:39 -0600

    ban user

--- app/models/banned_user.rb
+++ app/models/banned_user.rb
@@ -2,8 +2,8 @@ class BannedUser < ActiveRecord::Base
   attr_accessible :email, :user_id
   
   def initialize(user)
-      @banned_user.user_id = user.uid
-      @banned_user.email = user.email
-      @banned_user.save!   
+      banned_user.user_id = user.uid
+      banned_user.email = user.email
+      banned_user.save!   
   end
 end
