c80b76f31e1ce2bfdbc5896c5866f79926b87497
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-02-28 15:26:34 -0600

    ban user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
+    @banned_user = BannedUser.ban_user(user)
     @user.destroy
 
     respond_to do |format|
