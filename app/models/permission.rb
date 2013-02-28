e7fe4e612893ed87c3689486ace126dcf8544289
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-02-28 16:00:46 -0600

    authorization

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,6 +3,7 @@ class Permission < Struct.new(:user)
     if user.nil? || !user.authorized?
       return true if controller == "sesions"
       return true if controller == "home"
+      return true if controller == "auth"
     elsif user.authorized?
        return true if controller == "users" && action.in?(%w[index show edit new])
        return true if controller == "user_roles" && action.in?(%w[index show edit new])
@@ -12,6 +13,7 @@ class Permission < Struct.new(:user)
        return true if controller == "home" && action.in?(%w[index])
        return true if controller == "users" && action.in?(%w[index show edit new])
        return true if controller == "sesions" && action.in?(%w[create destroy failed])
+       return true if controller == "auth"
        return true if user.authorized?
        return true
     end
