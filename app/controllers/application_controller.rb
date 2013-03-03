3b90c9c29f130e95100cc5cfea2cb723d1705a6c
unknown <Koteshwar@Koteshwar-PC.(none)>
2013-03-03 14:04:23 -0600

    users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authorize
-  before_filter :require_user
   
   delegate :allow?, to: :current_permission
   helper_method :allow?
 
 private
-
-  def require_user
-    redirect_to root_url unless current_user
-  end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
